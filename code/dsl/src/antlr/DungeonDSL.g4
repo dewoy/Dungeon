f8ef3718a91353bd29ce85a94f0c7d050642e23a
mr <malt.r@mail.de>
2022-10-25 14:56:10 +0200

    squash later

--- code/dsl/src/antlr/DungeonDSL.g4
+++ code/dsl/src/antlr/DungeonDSL.g4
@@ -4,6 +4,128 @@ grammar DungeonDSL;
     package antlr.main;
 }
 
-program   : 'hello' ID;
-ID  : [a-z]+ ;
-WS  : [ \t\r\n]+ -> skip ;
+/*
+ * Lexer rules
+ */
+
+TYPE_SPECIFIER
+        : 'quest_config'
+        ;
+
+ID  : [_a-zA-Z][a-zA-Z0-9_]*;
+WS  : [ \t\r\n]+ -> skip;
+
+LINE_COMMENT
+        : '//' ~[\r\n]* -> channel(HIDDEN)
+        ;
+
+BLOCK_COMMENT
+        : '/*' .*? '*/' -> channel(HIDDEN)
+        ;
+
+/*
+ * Parser rules
+ */
+
+// TODO:
+// - expression grammar
+// - proper stmt definition
+
+program : obj_def+
+        //| fn_def
+        //| stmt
+        ;
+
+obj_def : TYPE_SPECIFIER ID '{' property_def '}'
+        | ('graph'|'digraph') ID '{' dot_def '}'
+        ;
+
+// TODO: make list (comma)
+property_def
+        : ID ':' stmt;
+
+// temporary, for testing
+stmt    : ID;
+/*
+ * -------------------- dot related definitions --------------------
+ * dot grammar: https://graphviz.org/doc/info/lang.html
+ *
+ * simplifications:
+ * - don't support subgraphs
+ * - don't support ports
+ */
+
+dot_def : dot_stmt_list?;
+
+dot_stmt_list
+        : dot_stmt ';'? dot_stmt_list?
+        ;
+
+dot_stmt
+        : dot_node_stmt
+        | dot_edge_stmt
+        | dot_attr_stmt
+        | ID '=' ID
+        ;
+
+dot_edge_stmt
+        : ID dot_edge_RHS dot_attr_list?
+        ;
+
+dot_edge_RHS
+        : dot_edge_op ID dot_edge_RHS?
+        ;
+
+// dot specifies the keywords as case insensitive,
+// we require them to be lowercase for simplicity
+dot_attr_stmt
+        : ('graph' | 'node' | 'edge') dot_attr_list
+        ;
+
+dot_node_stmt
+        : ID dot_attr_list?
+        ;
+
+dot_attr_list
+        : '[' dot_a_list? ']' dot_a_list?
+        ;
+
+dot_a_list
+        : ID '=' ID (';'|',')? dot_a_list?
+        ;
+
+dot_edge_op
+        : '->' #arrow_edge
+        | '--' #line_edge
+        ;
+
+// Reference Program
+/*
+graph g {
+  G--T
+  T--Q
+  Q--D
+  Q--F
+  T--X
+  X--S
+  G--W
+  W--E
+  W--C
+  C--U
+  C--N
+}
+
+quest_config conf {
+  style: tree_traversal,
+  type: dfs_pre_order,
+  tree: g,
+  points: 3,
+  task_desc: "Öffne die Schatztruhe mit dem korrekten Passwort! Das Passwort setzt
+  sich aus einzelnen Buchstaben zusammen, die in den Räumen des Dungeons zu
+  finden sind. Die Räume sind in einer Baumstruktur angeordnet. Der Raum mit der
+  Schatztruhe stellt die Wurzel des Baumes dar. Um diese Truhe zu öffnen muss
+  das Passwort in **Pre-Order** Reihenfolge angegeben werden.",
+}
+
+set_quest(conf)
+*/
---
+++ code/dsl/src/parser/DungeonDSLListener.java
@@ -0,0 +1,168 @@
+package parser;
+
+import antlr.main.DungeonDSLParser;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+public class DungeonDSLListener implements antlr.main.DungeonDSLListener {
+    @Override
+    public void enterProgram(DungeonDSLParser.ProgramContext ctx) {
+
+    }
+
+    @Override
+    public void exitProgram(DungeonDSLParser.ProgramContext ctx) {
+
+    }
+
+    @Override
+    public void enterObj_def(DungeonDSLParser.Obj_defContext ctx) {
+
+    }
+
+    @Override
+    public void exitObj_def(DungeonDSLParser.Obj_defContext ctx) {
+
+    }
+
+    @Override
+    public void enterProperty_def(DungeonDSLParser.Property_defContext ctx) {
+
+    }
+
+    @Override
+    public void exitProperty_def(DungeonDSLParser.Property_defContext ctx) {
+
+    }
+
+    @Override
+    public void enterStmt(DungeonDSLParser.StmtContext ctx) {
+
+    }
+
+    @Override
+    public void exitStmt(DungeonDSLParser.StmtContext ctx) {
+
+    }
+
+    @Override
+    public void enterDot_def(DungeonDSLParser.Dot_defContext ctx) {
+
+    }
+
+    @Override
+    public void exitDot_def(DungeonDSLParser.Dot_defContext ctx) {
+
+    }
+
+    @Override
+    public void enterDot_stmt_list(DungeonDSLParser.Dot_stmt_listContext ctx) {
+
+    }
+
+    @Override
+    public void exitDot_stmt_list(DungeonDSLParser.Dot_stmt_listContext ctx) {
+
+    }
+
+    @Override
+    public void enterDot_edge_stmt(DungeonDSLParser.Dot_edge_stmtContext ctx) {
+
+    }
+
+    @Override
+    public void exitDot_edge_stmt(DungeonDSLParser.Dot_edge_stmtContext ctx) {
+
+    }
+
+    @Override
+    public void enterDot_edge_RHS(DungeonDSLParser.Dot_edge_RHSContext ctx) {
+
+    }
+
+    @Override
+    public void exitDot_edge_RHS(DungeonDSLParser.Dot_edge_RHSContext ctx) {
+
+    }
+
+    @Override
+    public void enterDot_attr_stmt(DungeonDSLParser.Dot_attr_stmtContext ctx) {
+
+    }
+
+    @Override
+    public void exitDot_attr_stmt(DungeonDSLParser.Dot_attr_stmtContext ctx) {
+
+    }
+
+    @Override
+    public void enterDot_node_stmt(DungeonDSLParser.Dot_node_stmtContext ctx) {
+
+    }
+
+    @Override
+    public void exitDot_node_stmt(DungeonDSLParser.Dot_node_stmtContext ctx) {
+
+    }
+
+    @Override
+    public void enterDot_attr_list(DungeonDSLParser.Dot_attr_listContext ctx) {
+
+    }
+
+    @Override
+    public void exitDot_attr_list(DungeonDSLParser.Dot_attr_listContext ctx) {
+
+    }
+
+    @Override
+    public void enterDot_a_list(DungeonDSLParser.Dot_a_listContext ctx) {
+
+    }
+
+    @Override
+    public void exitDot_a_list(DungeonDSLParser.Dot_a_listContext ctx) {
+
+    }
+
+    @Override
+    public void enterArrow_edge(DungeonDSLParser.Arrow_edgeContext ctx) {
+
+    }
+
+    @Override
+    public void exitArrow_edge(DungeonDSLParser.Arrow_edgeContext ctx) {
+
+    }
+
+    @Override
+    public void enterLine_edge(DungeonDSLParser.Line_edgeContext ctx) {
+
+    }
+
+    @Override
+    public void exitLine_edge(DungeonDSLParser.Line_edgeContext ctx) {
+
+    }
+
+    @Override
+    public void visitTerminal(TerminalNode node) {
+
+    }
+
+    @Override
+    public void visitErrorNode(ErrorNode node) {
+
+    }
+
+    @Override
+    public void enterEveryRule(ParserRuleContext ctx) {
+
+    }
+
+    @Override
+    public void exitEveryRule(ParserRuleContext ctx) {
+
+    }
+}
